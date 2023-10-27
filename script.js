daf09393bc5542d7477d1edad729e75427ece6ce
Navya <88023981+navysai1419@users.noreply.github.com>
2023-10-27 15:47:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const container = document.getElementById('color-board');
+document.addEventListener('DOMContentLoaded', function () {
+    const container = document.getElementById('color-board');
 
-// Create 800 square elements
-for (let i = 0; i < 800; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square);
+    // Create 800 square elements
+    for (let i = 0; i < 800; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
 
-    // Add a transitionend event listener to each square to smoothly revert the color
-    square.addEventListener('transitionend', () => {
-        square.style.backgroundColor = 'lightgray';
-    });
-}
+        // Add an event listener for reverting to the original color after 1 second
+        square.addEventListener('mouseenter', function () {
+            const el = this;
+            el.style.backgroundColor = '#3498db';
+            setTimeout(function () {
+                el.style.backgroundColor = 'lightgray';
+            }, 1000);
+        });
+    }
+});
