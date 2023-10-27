e42998a2d7b8d039aff206dc33e21af22a90eb05
Navya <88023981+navysai1419@users.noreply.github.com>
2023-10-27 15:26:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-//your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function() {
-  const container = document.querySelector(".container");
+const container = document.getElementById('color-board');
 
-  // Create 800 boxes
-  for (let i = 0; i < 800; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
+// Create 800 square elements
+for (let i = 0; i < 800; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
     container.appendChild(square);
 
-    // Add an event listener to reset the color after 1 second
-    square.addEventListener("mouseover", function() {
-      setTimeout(function() {
-        square.style.backgroundColor = "rgb(29, 29, 29)";
-      }, 1000);
+    // Add a transitionend event listener to each square to smoothly revert the color
+    square.addEventListener('transitionend', () => {
+        square.style.backgroundColor = 'lightgray';
     });
-  }
-});
+}
